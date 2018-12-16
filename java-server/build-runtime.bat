9f5b2570a5357c2a046aee2609728ab4bb26cd45
Liiioooo <preissegger.leo@a1.net>
2018-12-16 22:25:04 +0100

    [TASK] updated build process now copying config.json, webDocs

--- java-server/build-runtime.bat
+++ java-server/build-runtime.bat
@@ -1,5 +1,8 @@
 rmdir dist /S /Q
 "%JAVA_HOME%\bin\jlink.exe" --module-path .\out\artifacts\LioFileTransfer_jar\LioFileTransfer.jar;"C:\Program Files\Java\jdk-11.0.1\jmods";.\lib\gson-modules.jar --add-modules LioFileTransfer --output ./dist --compress 2
 echo @echo off > ./dist/LioFileTransfer.bat
+echo title LioFileTransfer Server >> ./dist/LioFileTransfer.bat
 echo .\bin\java.exe -m LioFileTransfer/LioFileTransfer.Main >> ./dist/LioFileTransfer.bat
+copy config.json dist /y
+robocopy webDocs dist/webDocs /MIR
 echo PAUSE >> ./dist/LioFileTransfer.bat
